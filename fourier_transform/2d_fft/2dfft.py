8b5275333a6016c952bb2df9d01313b25c03f576
popikeyshen <popikeyshen@gmail.com>
2021-06-10 10:02:37 +0300

    2d_fft

--- fourier_transform/2d_fft/2dfft.py
+++ fourier_transform/2d_fft/2dfft.py
@@ -1,12 +1,57 @@
 
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
+from matplotlib import pyplot as plt
 
-# import the necessary packages
-import matplotlib.pyplot as plt
-import numpy as np
+def fft_to_image():
+	sp_matrix = np.zeros([100,100],dtype=int)
+	sp_matrix[95,95]=3000
+	sp_matrix[50,50]=3000
+	sp_matrix[25,25]=3000
 
-np.set_printoptions(threshold=np.inf)
+	plt.imshow(sp_matrix)
+	plt.show()
+
+	sig = np.fft.ifft(sp_matrix)
+
+	sig+=30
+	sig/=100.0
+
+	plt.plot(sig[50])
+	plt.imshow(abs(sig))
+	plt.show()
+
+
+def d1_sin_to_2dfft():
+	#1) create sin
+	x = np.linspace(0, 1, 100)
+	sin = (np.sin(x*10)+1)*100
+	plt.plot(sin)
+	plt.show()
+	sin = np.array(sin)
+
+	#2) add in to matrix
+	image = np.zeros([100,100],dtype=int)
+	image[:,50]=sin
+	image[:,49]=sin
+	image[:,48]=sin
+	image[:,47]=sin
+	image[:,46]=sin
+	
+	plt.imshow(image)
+	plt.show()
+
+
+	#3) sin image to spectre
+	sp = np.fft.fft(image)
+
+	plt.imshow(abs(sp))
+	plt.show()
+
+
+	sp_shifted = np.fft.fftshift(sp) # null freq is in center
+	plt.imshow(abs(sp_shifted))
+	plt.show()
 
 def detect_blur_fft(image=0, size=60, thresh=10, vis=True):
 	image  = cv2.imread("/home/popikeyshen/circle.jpg",0)
@@ -65,97 +110,6 @@ def detect_blur_fft(image=0, size=60, thresh=10, vis=True):
 	return (mean, mean <= thresh)
 
 
-def fft():
-	a1 = [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	a = np.array(a1)
-	a2 = np.gradient(a)
-	a = np.gradient(a2)
-	print(a)
-
-	t = np.arange(len(a))
-	sp = np.fft.fft(a)
-	freq = np.fft.fftfreq(t.shape[-1])
-	plt.plot(freq, sp.real, freq, sp.imag)
-
-	t = np.arange(256)
-	sp = np.fft.fft(np.sin(t))
-
-	freq = np.fft.fftfreq(t.shape[-1])
-
-	#plt.plot(np.sin(t))
-	plt.plot(a2)
-	plt.plot(a1)
-	plt.plot(a)
-	#plt.plot(freq, sp.real, freq, sp.imag)
-	plt.show()
-
-import cv2
-from matplotlib import pyplot as plt
-
-def grad():
-	img = cv2.imread("/home/popikeyshen/textures/1.jpg")
-	h,w,c = img.shape
-
-	for hh in range(h):
-		for ww in range(w):
-			bgr          = img[hh,ww]
-			bgr,g	     = distance(bgr)
-			img[hh,ww]   = bgr
-			gray[hh,ww]  = g
-	
-
-def image():
-	img  = cv2.imread("/home/popikeyshen/snr1.jpg",0).astype('float32')
-	img  = cv2.imread("/home/popikeyshen/convolution/test_conv.png",1)
-	img_gradient_x = np.gradient(img)[0]
-	img_gradient_y = np.gradient(img)[1]
-	#kernel = cv2.imread("/home/popikeyshen/eye.jpg",0).astype('float32')
-	#kernel_x = np.gradient(kernel)[0]
-	#kernel_y = np.gradient(kernel)[1]
-	#kernel_xy = kernel_x+kernel_y
-
-	img_gradient_xy = img_gradient_x+img_gradient_y
-	#print(img_gradient0)
-	#plt.imshow(img)
-	#img_gradient0 = abs(img_gradient0)
-	#kernel = abs(kernel)
-	plt.imshow(img_gradient_xy)
-	plt.show()
-
-
-
-	#res = np.convolve(img,img2)  // just 1d 
-	res = cv2.filter2D(img_gradient_xy,-1,kernel_xy)
-
-	#res[  res < 40000 ] =0
-
-	#print(img)
-	plt.imshow(res)
-	plt.show()
-
-	#from scipy import signal
-	#cor = signal.correlate2d (img, kernel)
-	#print(cor.shape)
-	#plt.imshow(cor)
-	#plt.show()
-
-	#cv2.imshow("res",res)
-	#cv2.waitKey(0)
-
-def fft_ifft():
-	a1 = [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	a = np.array(a1)
-
-	sp = np.fft.fft(a)
-	s = np.fft.ifft(sp)
-
-	plt.plot(a)
-	plt.show()
-	plt.plot(s)
-	plt.show()
-	plt.plot(sp)
-	plt.show()
-
 def fft_ifft_image():
 	a  = cv2.imread("/home/popikeyshen/circle.jpg",0)
 	a  = cv2.imread("/home/popikeyshen/convolution/test_conv.png",1)
@@ -204,35 +158,6 @@ def fft_ifft_image():
 
 	cv2.imshow("res",s.astype("uint8"))
 	cv2.waitKey(0)
-
-def kcf():
-	a  = cv2.imread("/home/popikeyshen/snr2.jpg",1)
-	b  = cv2.imread("/home/popikeyshen/eye.jpg",1)
-
-
-
-#fft()
-#detect_blur_fft()
-
-fft_ifft()
-#fft_ifft_image()
-
-#def full_conv():
-
-#full_conv()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
