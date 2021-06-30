738886027ee69cf0811012d990dbdbe70f6910f3
popikeyshen <popikeyshen@gmail.com>
2021-06-30 23:10:32 +0300

    signal filtering

---
+++ signal_filtering/README.md
@@ -0,0 +1,6 @@
+
+# signal filtering
+
+<p align="center"> <img src="1.png"  width = 50%  /></p>
+
+<p align="center"> <img src="2.png"  width = 50%  /></p>
---
+++ signal_filtering/filter_data.py
@@ -0,0 +1,96 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+
+
+
+def fft_filtering(vector, vis=True):
+
+
+	print(vector.shape)
+	size = int( 276/2 )
+
+	# compute the FFT to find the frequency transform, then shift
+	# the zero frequency component (i.e., DC component located at
+	# the top-left corner) to the center where it will be more
+	# easy to analyze
+	fft = np.fft.fft(vector)
+	fftShift = np.fft.fftshift(fft)
+
+	# check to see if we are visualizing our output
+	if vis:
+		# compute the magnitude spectrum of the transform
+		magnitude = 20 * np.log(np.abs(fftShift))
+		# display the original input vector
+		(fig, ax) = plt.subplots(1, 2, )
+		ax[0].plot(vector)
+		ax[0].set_title("Input")
+		ax[0].set_xticks([])
+		ax[0].set_yticks([])
+		# display the magnitude vector
+		ax[1].plot(magnitude)
+		ax[1].set_title("Magnitude Spectrum")
+		ax[1].set_xticks([])
+		ax[1].set_yticks([])
+		# show our plots
+
+
+		plt.show()
+
+	print(fftShift.shape)
+
+	# zero-out the center of the FFT shift (i.e., remove low
+	# frequencies), apply the inverse shift such that the DC
+	# component once again becomes the top-left, and then apply
+	# the inverse FFT
+
+	#fftShift[50:size-50] = 0
+
+	fftShift[:2] 		= 0
+	fftShift[size-2:]	= 0
+
+	fftShift = np.fft.ifftshift(fftShift)
+	recon = np.fft.ifft(fftShift)
+
+	res = vector - recon
+
+
+	if vis:
+
+		# display the original input vector
+		(fig, ax) = plt.subplots(1, 2, )
+		ax[0].plot(np.real(res))
+		ax[0].set_title("filtered signal")
+		ax[0].set_xticks([])
+		ax[0].set_yticks([])
+		# display the magnitude vector
+		ax[1].plot(np.real(recon))
+		ax[1].set_title("high frequency")
+		ax[1].set_xticks([])
+		ax[1].set_yticks([])
+		# show our plots
+
+
+		plt.show()
+
+
+	#print(np.abs(recon).shape)
+	# compute the magnitude spectrum of the reconstructed vector,
+	# then compute the mean of the magnitude values
+	magnitude = 20 * np.log(np.abs(recon))
+	mean = np.mean(magnitude)
+	print("high frequency mean", mean)
+
+if __name__ == "__main__":
+
+	#with open('signal.npy', 'wb') as f:
+	#	np.save(f, data_vector)
+
+	with open('signal.npy', 'rb') as f:
+		data_vector = np.load(f)
+
+
+	fft_filtering(data_vector)
+	#calman_filter():
+	#fft_filter():
