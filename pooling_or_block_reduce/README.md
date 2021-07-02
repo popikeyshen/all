ca672e50af4f5ed550eaef490031094e48c8d6fb
popikeyshen <popikeyshen@gmail.com>
2021-07-02 19:47:07 +0300

    avg, max, median pool

---
+++ pooling_or_block_reduce/README.md
@@ -0,0 +1,15 @@
+
+# Pooling layers or block reduce functions
+
+Scikit-image, keras, pytorch have block reduce.
+Keras has maxpooling, averagepooling techniques.
+With pytorch is possible to use power-average pooling, fractional or adaptive pooling layers.
+
+With scikit image and numpy statistics is possible to use median pooling, minimum, percentile, range, mean, std, etc. 
+```
+https://numpy.org/doc/stable/reference/routines.statistics.html
+https://keras.io/api/layers/
+```
+<p align="center"> <img src="cat.jpg"  width = 40%  /> </p>
+<p align="center"> <img src="avgpool10x.png"  width = 40%  /><img src="maxpool10x.png"  width = 40%  /><img src="median10x.png"  width = 40%  />   </p>
+
---
+++ pooling_or_block_reduce/pooling.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import skimage.measure
+#https://scikit-image.org/docs/dev/api/skimage.measure.html#skimage.measure.block_reduce
+
+
+def skimage_reduce(img):
+
+	h,w,c = img.shape
+	block_size= (10,10,1)
+	#block_size= (40,40,1)
+	#block_size= (h,w,1)
+	print(block_size)
+
+	#reduced = skimage.measure.block_reduce(img, block_size, np.max)
+	reduced = skimage.measure.block_reduce(img, block_size, np.average)
+
+
+	# 'numpy.float64' object cannot be interpreted as an integer
+	reduced = reduced.astype(np.uint8)
+
+
+	reduced = cv2.resize(reduced,(w,h))
+	cv2.imshow("reduced",reduced)
+	cv2.imshow("img",img)
+	cv2.waitKey(0)
+
+	#res = img-reduced.astype(np.float32)
+
+	return reduced
+
+
+if __name__ == "__main__":
+
+	img = cv2.imread("cat.jpg",1)
+
+	skimage_reduce(img)
