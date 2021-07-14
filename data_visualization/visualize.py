efdbf62c37a00ebf142ba014afd9c372e97e765d
popikeyshen <popikeyshen@gmail.com>
2021-07-14 05:45:15 +0300

    title plot

--- data_visualization/visualize.py
+++ data_visualization/visualize.py
@@ -42,6 +42,29 @@ def stem_vis():
 	plt.stem(index, func2, 'r', markerfmt='ro', label='f2')
 	plt.show()
 
+def title():
+
+	tx1= np.array([ 0,1,2,3,4,5,4,3,2,1,0])
+	tx2= np.array([ 0,1,0,1,0,1,0,1,0,1,0])
+
+	tx3 = tx1*tx2
+	tx4 = tx1+tx2
+
+
+	fig = plt.figure()
+	plt.title("signals")
+	plt.plot(tx1)
+	plt.plot(tx2)
+
+
+	fig = plt.figure()
+	plt.title("tx1+tx2, tx1*tx2")
+	plt.plot(tx3)
+	plt.plot(tx4)
+
+	plt.show()
+
+
 import torch
 import torch.nn as nn
 ### torch fast big size convolution
