bd09bbfee465e0ba7a10e0c261310cb2fe4d4255
popikeyshen <popikeyshen@gmail.com>
2021-08-18 12:53:02 +0300

    solid

---
+++ python/iterator_generator.py
@@ -0,0 +1,40 @@
+### https://devpractice.ru/python-lesson-15-iterators-and-generators/
+### создание итератора 
+class SimpleIterator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.counter = 0
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return 1
+        else:
+            raise StopIteration
+
+s_iter1 = SimpleIterator(3)
+print(next(s_iter1))
+print(next(s_iter1))
+print(next(s_iter1))
+print(next(s_iter1))
+
+
+### генераторы
+def simple_generator(val):
+   while val > 0:
+       val -= 1
+       yield 1
+
+gen_iter = simple_generator(5)
+print(next(gen_iter))
+print(next(gen_iter))
+print(next(gen_iter))
+print(next(gen_iter))
+print(next(gen_iter))
+print(next(gen_iter))
+
+
+
+
+
+
