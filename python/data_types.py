0d602dca15afbf9caaefdea3d3244a983d949d7c
popikeyshen <popikeyshen@gmail.com>
2021-09-21 13:53:43 +0300

    data types

---
+++ python/data_types.py
@@ -0,0 +1,19 @@
+
+
+
+a = {3,1,1,2,1}
+print(a)
+
+A = {1, 2, 3}
+B = {3, 2, 3, 1}
+print(A == B)
+
+B = {3, 2, 3, 1, 4}
+print(A == B)
+
+
+#### array  https://docs.python.org/3/library/array.html
+array('l')
+array('u', 'hello \u2641')
+array('l', [1, 2, 3, 4, 5])
+array('d', [1.0, 2.0, 3.14])
--- python/private.py
+++ python/private.py
@@ -13,3 +13,21 @@ foo.__private_variable=2021     ## not error
 print(foo.__private_variable)   ## not error
 #foo.insideclass()              ## error
 #foo.__private_method()         ## error
+
+
+class Student:
+    _sName = 'XYZ' # protected class attribute   # are also available to its sub-classes # https://www.tutorialsteacher.com/python/public-private-protected-modifiers
+    
+    def __init__(self, name, age):
+        self._name=name  # protected instance attribute
+        self._age=age # protected instance attribute
+
+
+class Student:
+    __sName = 'XYZ' # private class attribute
+
+    def __init__(self, name, age):
+        self.__name=name  # private instance attribute
+        self.__salary=age # private instance attribute
+    def __display(self):  # private method
+	    print('This is private method.')
