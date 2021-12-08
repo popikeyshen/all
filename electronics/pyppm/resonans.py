4da27e39079b9260d45ebf33578df0ef04c8314f
popikeyshen <popikeyshen@gmail.com>
2021-12-08 02:11:22 +0200

    nmr resonance code

--- electronics/pyppm/resonans.py
+++ electronics/pyppm/resonans.py
@@ -4,23 +4,38 @@ import numpy as np
 
 #http://chem.spbu.ru/files/Vladimir/Vasiliev/NMR_1.pdf
 
-## hyromagnetic constant
+## hyromagnetic constant for H^1
 y = 2.674*10**8
 
 B2 =  0.1         # N35	
-B1 = 30*10**(-6)  # normal earth magnetic field
+
+# 30-60 uT normal earth magnetic field
+B1 = 52*10**(-6)  # normal Kyiv earth magnetic field
+
+
 
 ## energy
 #E = B*h/(4*pi) 
 
 ## frequency
-v = y*B2/(2*np.pi)
+v = y*B1/(2*np.pi)
 
 print(v)
 
 
 '''
-1276 Hz  for 30 uTl
+1276 Hz  for 30 uT
+2213 Hz  for 52 uT
 4.2  Mhz for N35
 
 '''
+
+### for paper  https://www.bu.edu/chemistry/files/cic/nmr/documents/CICNMR_basicconcepts.pdf
+### inova 500 has 500 mhz frequency for H1
+### cause the B = 500 000 000 *4*pi / y
+B = 500000000 *2*np.pi / y
+print(B, 'T')  ## 11.74
+
+## The 500 MHz magnet system comprises a fully persistent 11.74 Tesla high
+## http://www.ietltd.com/pdf_datasheets/Unshielded%20Inova%20500%20Data%20Sheet.pdf
+
