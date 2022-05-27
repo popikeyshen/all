0be6c1e62962c1716f19e1f204cbf2d6b3bb177e
popikeyshen <popikeyshen@gmail.com>
2022-05-27 23:55:21 +0300

    deleting

--- Data_Science/svm_svr/svm/feature_to_svm/feature.py
+++ Data_Science/svm_svr/svm/feature_to_svm/feature.py
@@ -1,13 +1,7 @@
 
 import cv2
 import numpy as np
-import pickle
 
-def save_vec(vec_X,vec_y, name):
-	print('len ',len(vec_X), ', data save..')
-	if len(vec_X)>0:
-		with open(name+'.pickle', 'wb') as f_out:
-			pickle.dump((vec_X,vec_y), f_out)
 
 def data_to_vector(img, true_or_false):
 	_,_,c = img.shape
